1fe5fa0f92e2a869129bd2961e81adfe178f345e
overclokk <info@overclokk.net>
2020-01-23 15:58:01 +0100

    Tests

--- src/Hooks.php
+++ src/Hooks.php
@@ -41,4 +41,29 @@ class Hooks implements HooksInterface {
 	public function removeListener( string $event_name, callable $listener, int $priority = self::ORDER ) {
 		\remove_filter( ...\func_get_args() );
 	}
+
+	/**
+	 * Executes all the callbacks registered with the given hook.
+	 *
+	 * @param string $tag    The name of the action to be executed.
+	 * @param mixed  ...$arg Optional. Additional arguments which are passed on to the
+	 *                       functions hooked to the action. Default empty.
+	 * @return void
+	 */
+	public function execute( string $tag, ...$arg ) {
+		\do_action( ...\func_get_args() );
+	}
+
+	/**
+	 * Filters the given value by applying all the changes from the callbacks
+	 * registered with the given hook. Returns the filtered value.
+	 *
+	 * @param string $tag     The name of the filter hook.
+	 * @param mixed  $value   The value to filter.
+	 * @param mixed  ...$args Additional parameters to pass to the callback functions.
+	 * @return mixed The filtered value after all hooked functions are applied to it.
+	 */
+	public function filter( string $tag, $value, ...$args ) {
+		return \apply_filters( ...\func_get_args() );
+	}
 }
--- tests/unit/HooksTest.php
+++ tests/unit/HooksTest.php
@@ -90,4 +90,66 @@ class HooksTest extends \Codeception\Test\Unit {
 
 		$this->assertEquals( 1, $calls );
 	}
+
+	public function argumentsProvider() {
+		return [
+			'2 params passed'	=> [
+				[
+					'event_name',
+					'arg 1',
+				]
+			],
+			'3 params passed'	=> [
+				[
+					'event_name',
+					'arg 1',
+					'arg 2',
+				]
+			],
+			'4 params passed'	=> [
+				[
+					'event_name',
+					'arg 1',
+					'arg 2',
+					'arg 3',
+				]
+			],
+		];
+	}
+
+	/**
+	 * @test
+	 * @dataProvider argumentsProvider()
+	 */
+	public function itShouldExecuteWith( $args ) {
+		$sut = $this->getInstance();
+
+		// phpcs:ignore -- Method from Codeception
+		FunctionMockerLe\define('do_action', function () use (&$calls, $args) {
+			$calls++;
+			Assert::assertEquals($args, func_get_args());
+		});
+
+		$sut->execute( ...$args );
+
+		$this->assertEquals( 1, $calls );
+	}
+
+	/**
+	 * @test
+	 * @dataProvider argumentsProvider()
+	 */
+	public function itShouldFiltersWith( $args ) {
+		$sut = $this->getInstance();
+
+		// phpcs:ignore -- Method from Codeception
+		FunctionMockerLe\define('apply_filters', function () use (&$calls, $args) {
+			$calls++;
+			Assert::assertEquals($args, func_get_args());
+		});
+
+		$sut->filter( ...$args );
+
+		$this->assertEquals( 1, $calls );
+	}
 }
