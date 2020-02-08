811c1e9bcee3f53565ca29d869e736be4396cccd
overclokk <info@overclokk.net>
2020-02-08 19:44:26 +0100

    EventResolverExtension

--- tests/unit/ResolverExtensionTest.php
+++ tests/unit/ResolverExtensionTest.php
@@ -3,59 +3,66 @@ declare(strict_types=1);
 
 namespace ItalyStrap\Tests;
 
+use Codeception\Test\Unit;
+use ItalyStrap\Config\Config;
+use ItalyStrap\Empress\AurynResolverInterface;
 use ItalyStrap\Empress\Extension;
+use ItalyStrap\Empress\Injector;
+use ItalyStrap\Event\EventManager;
 use ItalyStrap\Event\EventResolverExtension;
+use Prophecy\Argument;
+use Prophecy\Prophecy\ObjectProphecy;
 
 // phpcs:disable
 require_once codecept_data_dir( '/fixtures/classes.php' );
 // phpcs:enable
-class ResolverExtensionTest extends \Codeception\Test\Unit {
+class ResolverExtensionTest extends Unit {
 
 	/**
 	 * @var \UnitTester
 	 */
 	protected $tester;
 	/**
-	 * @var \Prophecy\Prophecy\ObjectProphecy
+	 * @var ObjectProphecy
 	 */
 	private $fake_injector;
 
 	/**
-	 * @var \Prophecy\Prophecy\ObjectProphecy
+	 * @var ObjectProphecy
 	 */
 	private $event_manager;
 
 	/**
-	 * @var \Prophecy\Prophecy\ObjectProphecy
+	 * @var ObjectProphecy
 	 */
 	private $config;
 
 	/**
-	 * @return \ItalyStrap\Config\Config
+	 * @return Config
 	 */
-	public function getConfig(): \ItalyStrap\Config\Config {
+	public function getConfig(): Config {
 		return $this->config->reveal();
 	}
 
 	/**
-	 * @return \ItalyStrap\Event\EventManager
+	 * @return EventManager
 	 */
-	public function getEventManager(): \ItalyStrap\Event\EventManager {
+	public function getEventManager(): EventManager {
 		return $this->event_manager->reveal();
 	}
 
 	/**
-	 * @return \ItalyStrap\Empress\Injector
+	 * @return Injector
 	 */
-	public function getFakeInjector(): \ItalyStrap\Empress\Injector {
+	public function getFakeInjector(): Injector {
 		return $this->fake_injector->reveal();
 	}
 
 	// phpcs:ignore -- Method from Codeception
 	protected function _before() {
-		$this->fake_injector = $this->prophesize( \ItalyStrap\Empress\Injector::class );
-		$this->event_manager = $this->prophesize( \ItalyStrap\Event\EventManager::class );
-		$this->config = $this->prophesize( \ItalyStrap\Config\Config::class );
+		$this->fake_injector = $this->prophesize( Injector::class );
+		$this->event_manager = $this->prophesize( EventManager::class );
+		$this->config = $this->prophesize( Config::class );
 	}
 
 	// phpcs:ignore -- Method from Codeception
@@ -87,43 +94,83 @@ class ResolverExtensionTest extends \Codeception\Test\Unit {
 	/**
 	 * @test
 	 */
-	public function callbackShouldDoTheJobOfSubscribeListenersWithIndexedArray() {
-		$subscriber = $this->prophesize(\ItalyStrap\Tests\Subscriber::class );
+	public function callbackShouldSubscribeListenersWithIndexedArray() {
+		$subscriber = $this->prophesize( Subscriber::class );
 
 		$this->event_manager->addSubscriber( $subscriber->reveal() )->shouldBeCalled();
 		$this->config->get()->shouldNotBeCalled();
 
-		$this->fake_injector->share(\Prophecy\Argument::type('string'))
+		$this->fake_injector->share( Argument::type('string'))
 			->willReturn( $this->getFakeInjector() )
 			->shouldBeCalled();
 
-		$this->fake_injector->make(\Prophecy\Argument::type('string'))
+		$this->fake_injector->make( Argument::type('string'))
 			->willReturn( $subscriber->reveal() )
 			->shouldBeCalled();
 
 		$sut = $this->getInstance();
-		$sut->walk( \ItalyStrap\Tests\Subscriber::class, 0, $this->getFakeInjector() );
+		$sut->walk( Subscriber::class, 0, $this->getFakeInjector() );
 	}
 
 	/**
 	 * @test
 	 */
-//    public function callbackShouldDoTheJobOfSubscribeListenersWithAssociativedArray()
-//    {
-//		$subscriber = $this->prophesize(\ItalyStrap\Tests\Subscriber::class );
-//
-//		$this->event_manager->addSubscriber( $subscriber->reveal() )->shouldBeCalled();
-//		$this->config->get()->shouldNotBeCalled();
-//
-//		$this->fake_injector->share(\Prophecy\Argument::type('string'))
-//			->willReturn( $this->getFakeInjector() )
-//			->shouldBeCalled();
-//
-//		$this->fake_injector->make(\Prophecy\Argument::type('string'))
-//			->willReturn( $subscriber->reveal() )
-//			->shouldBeCalled();
-//
-//		$sut = $this->getInstance();
-//		$sut->walk( \ItalyStrap\Tests\Subscriber::class, '0', $this->getFakeInjector() );
-//    }
+	public function callbackShouldSubscribeListenersFormAssociativeArrayWithTrueOptionKey() {
+		$subscriber = $this->prophesize( Subscriber::class );
+		$config = [
+			'key'	=> true
+		];
+		$key = \array_keys( $config )[0];
+
+		$this->event_manager->addSubscriber( $subscriber->reveal() )->shouldBeCalled();
+		$this->config->get( $key, false )->willReturn($config[$key])->shouldBeCalled();
+
+		$this->fake_injector->share( Argument::type('string'))
+			->willReturn( $this->getFakeInjector() )
+			->shouldBeCalled();
+
+		$this->fake_injector->make( Argument::type('string'))
+			->willReturn( $subscriber->reveal() )
+			->shouldBeCalled();
+
+		$sut = $this->getInstance();
+		$sut->walk( Subscriber::class, $key, $this->getFakeInjector() );
+	}
+
+	/**
+	 * @test
+	 */
+	public function callbackShouldNotSubscribeListenersFromAssociativeArrayWithFalseOptionKey() {
+		$subscriber = $this->prophesize( Subscriber::class );
+		$config = [
+			'key'	=> false
+		];
+		$key = \array_keys( $config )[0];
+
+		$this->event_manager->addSubscriber( $subscriber->reveal() )->shouldNotBeCalled();
+		$this->config->get( $key, false )->willReturn($config[$key])->shouldBeCalled();
+
+		$this->fake_injector->share( Argument::type('string'))
+			->willReturn( $this->getFakeInjector() )
+			->shouldNotBeCalled();
+
+		$this->fake_injector->make( Argument::type('string'))
+			->willReturn( $subscriber->reveal() )
+			->shouldNotBeCalled();
+
+		$sut = $this->getInstance();
+		$sut->walk( Subscriber::class, $key, $this->getFakeInjector() );
+	}
+
+	/**
+	 * @test
+	 */
+	public function itShouldExecute() {
+		$application = $this->prophesize(AurynResolverInterface::class);
+
+		$application->walk(Argument::type('string'), Argument::type('callable'))->shouldBeCalled();
+
+		$sut = $this->getInstance();
+		$sut->execute($application->reveal());
+	}
 }
